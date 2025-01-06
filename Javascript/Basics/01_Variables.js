ecf8418d3d37cef9ab668a414b5ff48d6914bd5d
safarmansuri <safar.mansuri19@gmail.com>
2025-01-06 12:33:44 +0530

    Changes in Datatypes

---
+++ Javascript/Basics/01_Variables.js
@@ -0,0 +1,25 @@
+const accountId = 23456;
+let accountName = "Safaraccount";
+var accountPassWord = "sdfsdf";
+accountState = "";
+let accountAddress;
+
+// accountId = 2345; // NOT ALLOWED
+
+// Apart from const let var or the variable without any keyword are changable
+
+
+// if inintial value is not given than the variable is marked as a undefined.
+
+// prefer not to use var
+// because of the scope issue(Blocked scope and functional scope)
+
+
+console.table([accountId,accountName,accountPassWord,accountState,accountAddress]);
+
+
+
+console.log(Hoisting)
+const Hoisting = 2;
+
+  
---
+++ Javascript/Basics/02_DataTypes.js
@@ -0,0 +1,61 @@
+/*
+number
+string
+boolean
+null
+undefined
+symbol 
+*/
+
+// typeof  => using this we can see the type of the variable
+
+//********oprators ***************** */\\\
+
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2/2)
+// console.log(2%2)
+// console.log(2*2)
+// console.log(2**3)
+
+// type conversion in js
+
+// console.log(1 +  "2");   //12
+// console.log("1" +  "2");  //12
+// console.log("1" +  2);   //12
+// console.log("1" +  2 + 2); // 122
+// console.log(1 +  2 + "2"); // 32
+
+// Example of all the data types
+
+
+let number = 3 // number
+
+let name = "safar";  //string
+
+let isTrue = true;  // boolean
+ 
+const person = {firstname: "safar", lastName: "Mansuri" } // object
+
+const card = ["Ford", "Renault", "hyundai"]
+
+const date = new Date("2025-01-05")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
